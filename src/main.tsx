e990af0b5b4dd43c94e22309047a321537e61885
Mark Burggraf <markb@mantisbible.com>
2026-01-03 15:46:18 -0800

    feat: integrate Google OAuth provider into app

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import { ConfigProvider } from './contexts/ConfigContext'
 import { AuthProvider } from './contexts/AuthContext'
 import { ChurchProvider } from './contexts/ChurchContext'
 import { ThemeProvider } from './contexts/ThemeContext'
+import { GoogleAuthProvider } from './contexts/GoogleAuthContext'
 import { AppRoutes } from './routes'
 import './i18n'
 import './index.css'
+
+const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || ''
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -14,7 +18,11 @@ createRoot(document.getElementById('root')!).render(
       <ConfigProvider>
         <AuthProvider>
           <ChurchProvider>
-            <AppRoutes />
+            <GoogleOAuthProvider clientId={googleClientId}>
+              <GoogleAuthProvider>
+                <AppRoutes />
+              </GoogleAuthProvider>
+            </GoogleOAuthProvider>
           </ChurchProvider>
         </AuthProvider>
       </ConfigProvider>
